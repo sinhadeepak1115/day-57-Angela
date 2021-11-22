5e72c1a55974ec85d5e95fe6ee4add63352e2e50
Deepak Sinha <sinhadeepak1115study@gmail.com>
2021-11-23 00:34:58 +0530

    make a age and gender gusser using falsk in python.

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return "Plzz enter your name at the end of the url."
 
+@app.route("/<name>")
+def genz(name):
+    requests_age = requests.get(f"https://api.agify.io/?name={name}").json()
+    age = (requests_age.get("age"))
+    requests_gender = requests.get(f"https://api.genderize.io/?name={name}").json()
+    gender = (requests_gender.get("gender"))
+    return render_template("index.html",name_1=name, age=age, gender=gender)
 
 if __name__ == "__main__":
     app.run(debug=True)
